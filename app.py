c70479184b0ab3bc92b478d30384e569bb277565
Florin Langer <florin@berkeley.edu>
2018-02-23 14:16:16 -0800

    added plot

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ colors = {
 }
 
 gdf = gpd.read_file('data/pov_emissionsFresno.shp')
+gdf2 = gpd.read_file('data/pov_emissionsFresno_0.shp')
 
 markdown_text = '.'
 
@@ -42,11 +43,11 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     #     'color': colors['text']
     # }),
 
-    html.H3(children='Poverty Percentile to PM2.5 Emissions Scatter Plot', style={
+
+    html.H3(children='Poverty Percentile to PM2.5 Emissions Scatter Plot over Blockgroups', style={
         'textAlign': 'left',
         'color': colors['H3']
     }),
-
     dcc.Dropdown(
         id='pov-emissions-dropdown',
         options=[
@@ -83,13 +84,55 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         }
     ),
 
+
+    html.H3(children='Poverty to Asthma Percentile PM2.5 Scatter Plot over Blockgroups', style={
+        'textAlign': 'left',
+        'color': colors['H3']
+    }),
+    dcc.Dropdown(
+        id='pov-emissions-dropdown2',
+        options=[
+            {'label': 'Fresno', 'value': 'Fresno'},
+            {'label': 'Los Angeles', 'value': 'LA'}
+        ],
+        value='Fresno'
+    ),
+    dcc.Graph(
+        id='pov-emissions2',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=gdf2[gdf2['Tract_1'] == i]['Pov_pctl'],
+                    y=gdf2[gdf2['Tract_1'] == i]['Asthma_Pct'],
+                    # text=gdf[gdf['Tract_1'] == i]['Tract_1'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': gdf2[gdf2['Tract_1'] == i]['PM2.5T'],
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in gdf2.Tract_1.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
+                yaxis={'title': 'Asthma Percentile (Statewide)'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest',
+                # title='\nFresno Poverty Percentile to PM2.5 Emissions Scatter Plot'
+            )
+        }
+    ),
+
+
     html.Div(style={
         'textAlign': 'left',
         'color': colors['text'],
-        'width': '600', 'height': '600'
+        'width': '40%', 'height': '500'
         },
         children=[
-            html.Iframe(style={'border': 'none', 'width': '100%', 'height': '100%'},srcDoc='<style>.embed-container {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="pop health" src="//www.arcgis.com/apps/Embed/index.html?webmap=f66e9be609df4049967e36c1de32f128&amp;extent=-123.1827,35.4058,-116.0966,37.8868&zoom=true&previewImage=false&scale=true&legend=true&disable_scroll=true&theme=light"></iframe></div>'),
+            html.Iframe(style={'border': 'none', 'width': '100%', 'height': '100%'},srcDoc='<style>.embed-container {position: relative; padding-bottom: 67%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="pop health" src="//www.arcgis.com/apps/Embed/index.html?webmap=f66e9be609df4049967e36c1de32f128&amp;extent=-123.1827,35.4058,-116.0966,37.8868&zoom=true&previewImage=false&scale=true&legend=true&disable_scroll=true&theme=light"></iframe></div>'),
         ]
     ),
 
@@ -182,6 +225,40 @@ def update_graph(selected_dropdown_value):
         'layout': go.Layout(
             xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
             yaxis={'title': 'PM2.5 (Raw)'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            # title='\nFresno Poverty Percentile to PM2.5 Emissions Scatter Plot'
+        )
+    }
+
+
+@app.callback(Output('pov-emissions2', 'figure'), [Input('pov-emissions-dropdown2', 'value')])
+def update_graph(selected_dropdown_value):
+    gdf2 = gpd.read_file('data/pov_emissions' + selected_dropdown_value + '_0.shp')
+
+        # web.DataReader(
+        # selected_dropdown_value, data_source='google',
+        # start=dt(2017, 1, 1), end=dt.now()
+        # )
+    return {
+        'data': [
+            go.Scatter(
+                x=gdf2[gdf2['Tract_1'] == i]['Pov_pctl'],
+                y=gdf2[gdf2['Tract_1'] == i]['Asthma_Pct'],
+                # text=gdf[gdf['Tract_1'] == i]['Tract_1'],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': gdf2[gdf2['Tract_1'] == i]['PM2.5T'],
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name=i
+            ) for i in gdf2.Tract_1.unique()
+        ],
+        'layout': go.Layout(
+            xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
+            yaxis={'title': 'Asthma Percentile (Statewide)'},
             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             legend={'x': 0, 'y': 1},
             hovermode='closest',
