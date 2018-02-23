e706dd0db727b775b58207d42c2caa26d51b01f5
Florin Langer <florin@berkeley.edu>
2018-02-22 23:56:04 -0800

    added plots

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import os
 
 import pandas as pd
 import geopandas as gpd
 
-app = dash.Dash()
+app = dash.Dash(__name__)
+server = app.server
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 colors = {
     'background': '#FFFFFF',
