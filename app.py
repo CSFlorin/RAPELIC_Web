fa8f86ad21b9b41093c9c6b3490d0d6852c37d56
Florin Langer <florin@berkeley.edu>
2018-03-16 00:27:43 -0700

    added style

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ markdown_text = '.'
 
 app.layout = html.Div(className="everything", style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Pollution Health Damages Reduction in California',
+        children='Reduction of Pollutant Health Damages in California',
         style={
             'textAlign': 'center',
             'color': colors['gray']
@@ -58,57 +58,93 @@ app.layout = html.Div(className="everything", style={'backgroundColor': colors['
                         className="six columns",
                         children=[
                             html.Div(
-                                children=
+                                children=[
+                                html.H4(children='Poverty Percentile to PM2.5 Emissions Scatter Plot over Blockgroups', style={
+                                    'textAlign': 'left',
+                                    'color': colors['black']
+                                }),
+                                dcc.Dropdown(
+                                    id='pov-emissions-dropdown',
+                                    options=[
+                                        {'label': 'Fresno', 'value': 'Fresno'},
+                                        {'label': 'Los Angeles', 'value': 'LA'}
+                                    ],
+                                    value='Fresno'
+                                ),
                                 dcc.Graph(
-                                    id='left-graph',
+                                    id='pov-emissions',
                                     figure={
-                                        'data': [{
-                                            'x': [1, 2, 3],
-                                            'y': [3, 1, 2],
-                                            'type': 'bar'
-                                        }],
-                                        'layout': {
-                                            'height': 800,
-                                            'margin': {
-                                                'l': 10, 'b': 20, 't': 0, 'r': 0
-                                            }
-                                        }
+                                        'data': [
+                                            go.Scatter(
+                                                x=gdf[gdf['Tract_1'] == i]['Pov_pctl'],
+                                                y=gdf[gdf['Tract_1'] == i]['PM2.5T'],
+                                                # text=gdf[gdf['Tract_1'] == i]['Tract_1'],
+                                                mode='markers',
+                                                opacity=0.7,
+                                                marker={
+                                                    'size': 15,
+                                                    'line': {'width': 0.5, 'color': 'white'}
+                                                },
+                                                name=i
+                                            ) for i in gdf.Tract_1.unique()
+                                        ],
+                                        'layout': go.Layout(
+                                            xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
+                                            yaxis={'title': 'PM2.5 (Raw)'},
+                                            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                                            legend={'x': 0, 'y': 1},
+                                            hovermode='closest',
+                                            # title='\nFresno Poverty Percentile to PM2.5 Emissions Scatter Plot'
+                                        )
                                     }
                                 )
+                                ]
                             )
                         ]
                     ),
+
                     html.Div(
                         className="six columns",
                         children=html.Div([
-                            dcc.Graph(
-                                id='right-top-graph',
-                                figure={
-                                    'data': [{
-                                        'x': [1, 2, 3],
-                                        'y': [3, 1, 2],
-                                        'type': 'bar'
-                                    }],
-                                    'layout': {
-                                        'height': 400,
-                                        'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
-                                    }
-                                }
+                            html.H4(children='Poverty to Asthma Percentile PM2.5 Scatter Plot over Blockgroups', style={
+                                'textAlign': 'left',
+                                'color': colors['black']
+                            }),
+                            dcc.Dropdown(
+                                id='pov-emissions-dropdown2',
+                                options=[
+                                    {'label': 'Fresno', 'value': 'Fresno'},
+                                    {'label': 'Los Angeles', 'value': 'LA'}
+                                ],
+                                value='Fresno'
                             ),
                             dcc.Graph(
-                                id='right-bottom-graph',
+                                id='pov-emissions2',
                                 figure={
-                                    'data': [{
-                                        'x': [1, 2, 3],
-                                        'y': [3, 1, 2],
-                                        'type': 'bar'
-                                    }],
-                                    'layout': {
-                                        'height': 400,
-                                        'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
-                                    }
+                                    'data': [
+                                        go.Scatter(
+                                            x=gdf2[gdf2['Tract_1'] == i]['Pov_pctl'],
+                                            y=gdf2[gdf2['Tract_1'] == i]['Asthma_Pct'],
+                                            # text=gdf[gdf['Tract_1'] == i]['Tract_1'],
+                                            mode='markers',
+                                            opacity=0.7,
+                                            marker={
+                                                'size': gdf2[gdf2['Tract_1'] == i]['PM2.5T'],
+                                                'line': {'width': 0.5, 'color': 'white'}
+                                            },
+                                            name=i
+                                        ) for i in gdf2.Tract_1.unique()
+                                    ],
+                                    'layout': go.Layout(
+                                        xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
+                                        yaxis={'title': 'Asthma Percentile (Statewide)'},
+                                        margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                                        legend={'x': 0, 'y': 1},
+                                        hovermode='closest',
+                                        # title='\nFresno Poverty Percentile to PM2.5 Emissions Scatter Plot'
+                                    )
                                 }
-                            ),
+                            )
 
                         ])
                     )
@@ -117,98 +153,47 @@ app.layout = html.Div(className="everything", style={'backgroundColor': colors['
         ]
     ),
 
-
-    html.H4(children='Poverty Percentile to PM2.5 Emissions Scatter Plot over Blockgroups', style={
-        'textAlign': 'left',
-        'color': colors['black']
-    }),
-    dcc.Dropdown(
-        id='pov-emissions-dropdown',
-        options=[
-            {'label': 'Fresno', 'value': 'Fresno'},
-            {'label': 'Los Angeles', 'value': 'LA'}
-        ],
-        value='Fresno'
-    ),
-    dcc.Graph(
-        id='pov-emissions',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=gdf[gdf['Tract_1'] == i]['Pov_pctl'],
-                    y=gdf[gdf['Tract_1'] == i]['PM2.5T'],
-                    # text=gdf[gdf['Tract_1'] == i]['Tract_1'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in gdf.Tract_1.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
-                yaxis={'title': 'PM2.5 (Raw)'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest',
-                # title='\nFresno Poverty Percentile to PM2.5 Emissions Scatter Plot'
-            )
-        }
-    ),
-
-
-    html.H4(children='Poverty to Asthma Percentile PM2.5 Scatter Plot over Blockgroups', style={
-        'textAlign': 'left',
-        'color': colors['black']
-    }),
-    dcc.Dropdown(
-        id='pov-emissions-dropdown2',
-        options=[
-            {'label': 'Fresno', 'value': 'Fresno'},
-            {'label': 'Los Angeles', 'value': 'LA'}
-        ],
-        value='Fresno'
-    ),
-    dcc.Graph(
-        id='pov-emissions2',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=gdf2[gdf2['Tract_1'] == i]['Pov_pctl'],
-                    y=gdf2[gdf2['Tract_1'] == i]['Asthma_Pct'],
-                    # text=gdf[gdf['Tract_1'] == i]['Tract_1'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': gdf2[gdf2['Tract_1'] == i]['PM2.5T'],
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in gdf2.Tract_1.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'Poverty Percentile (Statewide)'},
-                yaxis={'title': 'Asthma Percentile (Statewide)'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest',
-                # title='\nFresno Poverty Percentile to PM2.5 Emissions Scatter Plot'
-            )
-        }
-    ),
-
-
-    html.Div(style={
-        'textAlign': 'left',
-        'color': colors['text'],
-        'width': '40%', 'height': '500'
-        },
+    html.Div(
+        className="row",
         children=[
-            html.Iframe(style={'border': 'none', 'width': '100%', 'height': '100%'},srcDoc='<style>.embed-container {position: relative; padding-bottom: 67%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="pop health" src="//www.arcgis.com/apps/Embed/index.html?webmap=f66e9be609df4049967e36c1de32f128&amp;extent=-123.1827,35.4058,-116.0966,37.8868&zoom=true&previewImage=false&scale=true&legend=true&disable_scroll=true&theme=light"></iframe></div>'),
+            html.Div(
+                className="six columns",
+                children=[
+                    html.Div(
+                        children=[
+                        html.H4(children='Poverty Percentile over Census Tracts', style={
+                            'textAlign': 'left',
+                            'color': colors['black']
+                        }),
+                        html.Div(style={
+                            'textAlign': 'left',
+                            'color': colors['text'],
+                            # 'width': '40%',
+                            'height': '500'
+                            },
+                            children=[
+                                html.Iframe(style={'border': 'none', 'width': '100%', 'height': '100%'},srcDoc='<style>.embed-container {position: relative; padding-bottom: 67%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="pop health" src="//www.arcgis.com/apps/Embed/index.html?webmap=f66e9be609df4049967e36c1de32f128&amp;extent=-123.1827,35.4058,-116.0966,37.8868&zoom=true&previewImage=false&scale=true&legend=true&disable_scroll=true&theme=light"></iframe></div>'),
+                            ]
+                        )
+                        ]
+                    )
+                ]
+            ),
+
+            html.Div(
+                className="six columns",
+                children=html.Div([
+                    html.P(children='A description\nof\nthe\nmap\non\nthe\nleft', style={
+                        'textAlign': 'left',
+                        'color': colors['gray']
+                    })
+
+                ])
+            )
         ]
     ),
+
+
 
     # dcc.Graph(
     #     id='pov-emissions-map',
@@ -237,11 +222,10 @@ app.layout = html.Div(className="everything", style={'backgroundColor': colors['
     #     }
     # ),
 
-    html.H3(children='Example', style={
+    html.H4(children='Example', style={
         'textAlign': 'left',
-        'color': colors['H3']
+        'color': colors['black']
     }),
-
     dcc.Graph(
         id='example-graph-2',
         figure={
